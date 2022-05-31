060ea8776f3104a734e0eb92eb664138afdcccf8
iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>
2022-05-31 18:58:21 +0200

    Bump emulated application version
    Signed-off-by: iTaysonLab <9827543+iTaysonLab@users.noreply.github.com>

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/core/util/SpUtils.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/core/util/SpUtils.kt
@@ -5,7 +5,7 @@ import android.os.Build
 import android.provider.Settings
 
 object SpUtils {
-    const val SPOTIFY_APP_VERSION = "8.7.32.3"
+    const val SPOTIFY_APP_VERSION = "8.7.38.86"
 
     fun getDeviceName(appContext: Context): String {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
