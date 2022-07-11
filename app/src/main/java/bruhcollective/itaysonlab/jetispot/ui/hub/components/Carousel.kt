0f73adf9e0826fc0376475c07af5c5f1086c2d29
DotsTeaLab <dotsTeaLab@gmai.com>
2022-07-11 16:41:15 +0200

    redesigned rows you see at the bottom of artists page

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/Carousel.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/Carousel.kt
@@ -2,22 +2,26 @@ package bruhcollective.itaysonlab.jetispot.ui.hub.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
 import bruhcollective.itaysonlab.jetispot.core.objs.hub.HubItem
+import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
 import bruhcollective.itaysonlab.jetispot.ui.hub.HubBinder
 import bruhcollective.itaysonlab.jetispot.ui.hub.HubScreenDelegate
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Carousel(
   navController: LambdaNavigationController,
@@ -26,13 +30,25 @@ fun Carousel(
 ) {
   Column(Modifier.padding(vertical = if (delegate.isSurroundedWithPadding()) 0.dp else 8.dp)) {
     if (item.text != null) {
-      Text(text = item.text.title!!, color = MaterialTheme.colorScheme.onSurface, fontWeight = FontWeight.Bold, fontSize = 16.sp,
-        modifier = Modifier.padding(horizontal = if (delegate.isSurroundedWithPadding()) 0.dp else 16.dp).padding(bottom = 12.dp))
+      Text(
+        text = item.text.title!!,
+        color = MaterialTheme.colorScheme.onSurface,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        modifier = Modifier
+          .padding(horizontal = if (delegate.isSurroundedWithPadding()) 0.dp else 16.dp)
+          .padding(top = 8.dp, bottom = 12.dp)
+      )
     }
 
-    LazyRow(horizontalArrangement = Arrangement.spacedBy(12.dp), modifier = Modifier.padding(horizontal = if (delegate.isSurroundedWithPadding()) 0.dp else 16.dp)) {
-      items(item.children ?: listOf()) { cItem ->
-        HubBinder(navController, delegate, cItem)
+    Card(shape = RoundedCornerShape(32.dp), modifier = Modifier.padding(horizontal = 16.dp)) {
+      LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        contentPadding = PaddingValues(22.dp)
+      ) {
+        items(item.children ?: listOf()) { cItem ->
+          HubBinder(navController, delegate, cItem)
+        }
       }
     }
   }
--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/MediumCard.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/MediumCard.kt
@@ -1,24 +1,28 @@
 package bruhcollective.itaysonlab.jetispot.ui.hub.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
+import androidx.compose.ui.unit.sp
 import bruhcollective.itaysonlab.jetispot.core.objs.hub.HubItem
+import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
 import bruhcollective.itaysonlab.jetispot.ui.hub.HubScreenDelegate
 import bruhcollective.itaysonlab.jetispot.ui.hub.clickableHub
-import bruhcollective.itaysonlab.jetispot.ui.shared.MediumText
 import bruhcollective.itaysonlab.jetispot.ui.shared.PreviewableAsyncImage
-import bruhcollective.itaysonlab.jetispot.ui.shared.Subtext
-import coil.compose.AsyncImage
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalTextApi::class)
 @Composable
 fun MediumCard(
   navController: LambdaNavigationController,
@@ -26,23 +30,65 @@ fun MediumCard(
   item: HubItem
 ) {
   val size = 160.dp
-  
-  Column(Modifier.width(size).clickableHub(navController, delegate, item)) {
-    var drawnTitle = false
 
-    PreviewableAsyncImage(imageUrl = item.images?.main?.uri, placeholderType = item.images?.main?.placeholder, modifier = Modifier.size(size).clip(
-      RoundedCornerShape(if (item.images?.main?.isRounded == true) 12.dp else 0.dp)
-    ))
+  Surface(
+    color = MaterialTheme.colorScheme.surface,
+    shape = RoundedCornerShape(20.dp)
+  ) {
+    Column(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = Modifier
+        .width(size)
+        .clickableHub(navController, delegate, item)
+        .padding(bottom = 10.dp)
+    ) {
+      var drawnTitle = false
 
-    if (!item.text?.title.isNullOrEmpty()) {
-      drawnTitle = true
-      MediumText(item.text!!.title!!, modifier = Modifier.padding(top = 8.dp))
-    }
+      PreviewableAsyncImage(
+        imageUrl = item.images?.main?.uri,
+        placeholderType = item.images?.main?.placeholder,
+        modifier = Modifier
+          .padding(top = 8.dp)
+          .size(144.dp)
+          .clip(RoundedCornerShape(if (item.images?.main?.isRounded == true) 12.dp else 16.dp))
+      )
 
-    if (!item.text?.subtitle.isNullOrEmpty()) {
-      Subtext(item.text!!.subtitle!!, modifier = Modifier.padding(top = if (drawnTitle) 4.dp else 8.dp))
-    } else if (!item.text?.description.isNullOrEmpty()) {
-      Subtext(item.text!!.description!!, modifier = Modifier.padding(top = if (drawnTitle) 4.dp else 8.dp))
+      Column(
+        Modifier.height(68.dp).padding(horizontal = 8.dp),
+        verticalArrangement = Arrangement.Center
+      ) {
+        if (!item.text?.title.isNullOrEmpty()) {
+          drawnTitle = true
+          Text(
+            item.text!!.title!!,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(top = 8.dp, start = 0.dp),
+            color = MaterialTheme.colorScheme.onSecondaryContainer,
+            style = TextStyle(platformStyle = PlatformTextStyle(false))
+          )
+        }
+
+        if (!item.text?.subtitle.isNullOrEmpty()) {
+          Text(
+            item.text!!.subtitle!!,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+              .padding(top = if (drawnTitle) 4.dp else 8.dp)
+              .padding(start = 8.dp),
+            style = TextStyle(platformStyle = PlatformTextStyle(false))
+          )
+        } else if (!item.text?.description.isNullOrEmpty()) {
+          Text(
+            item.text!!.description!!,
+            fontSize = 12.sp,
+            modifier = Modifier.padding(top = if (drawnTitle) 4.dp else 8.dp),
+            style = TextStyle(platformStyle = PlatformTextStyle(false)),
+            maxLines = 2
+          )
+        }
+      }
     }
   }
 }
