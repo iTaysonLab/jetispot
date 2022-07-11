3f9de399ea0a06b2149c6fd362b25bcaa151cb58
DotsTeaLab <dotsTeaLab@gmai.com>
2022-07-12 01:48:56 +0200

    large top app bar on home screen

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/ToolbarComponentBinder.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/ToolbarComponentBinder.kt
@@ -5,34 +5,38 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.History
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.material.icons.rounded.Settings
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
 import bruhcollective.itaysonlab.jetispot.ui.ext.dynamicUnpack
 import com.spotify.home.dac.component.v1.proto.ToolbarComponent
+import com.spotify.home.dac.component.v1.proto.ToolbarItemFeedComponent
 import com.spotify.home.dac.component.v1.proto.ToolbarItemListeningHistoryComponent
 import com.spotify.home.dac.component.v1.proto.ToolbarItemSettingsComponent
-import com.spotify.home.dac.component.v1.proto.ToolbarItemFeedComponent
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ToolbarComponentBinder(
   navController: LambdaNavigationController,
   item: ToolbarComponent
 ) {
-  SmallTopAppBar(title = {
-    Text(item.dayPartMessage)
-  }, actions = {
-    item.itemsList.forEach {
-      when (val protoItem = it.dynamicUnpack()) {
-        is ToolbarItemFeedComponent -> ToolbarItem(navController, Icons.Rounded.Notifications, protoItem.navigateUri, protoItem.title)
-        is ToolbarItemListeningHistoryComponent -> ToolbarItem(navController, Icons.Rounded.History, protoItem.navigateUri, protoItem.title)
-        is ToolbarItemSettingsComponent -> ToolbarItem(navController, Icons.Rounded.Settings, protoItem.navigateUri, protoItem.title)
+  LargeTopAppBar(
+    title = { Text(item.dayPartMessage) },
+    actions = {
+      item.itemsList.forEach {
+        when (val protoItem = it.dynamicUnpack()) {
+          is ToolbarItemFeedComponent -> ToolbarItem(navController, Icons.Rounded.Notifications, protoItem.navigateUri, protoItem.title)
+          is ToolbarItemListeningHistoryComponent -> ToolbarItem(navController, Icons.Rounded.History, protoItem.navigateUri, protoItem.title)
+          is ToolbarItemSettingsComponent -> ToolbarItem(navController, Icons.Rounded.Settings, protoItem.navigateUri, protoItem.title)
+        }
       }
-    }
-  }, modifier = Modifier.statusBarsPadding())
+    },
+    modifier = Modifier.statusBarsPadding()
+  )
 }
 
 @Composable
@@ -42,9 +46,7 @@ private fun ToolbarItem(
   navigateTo: String,
   contentDesc: String
 ) {
-  IconButton(onClick = {
-    navController.navigate(navigateTo)
-  }) {
+  IconButton(onClick = { navController.navigate(navigateTo) }) {
     Icon(icon, contentDescription = contentDesc)
   }
 }
