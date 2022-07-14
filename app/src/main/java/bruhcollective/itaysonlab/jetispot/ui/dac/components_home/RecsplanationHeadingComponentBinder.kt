9e13a70dea8b9859a01ee8082c67c0fba381b654
DotsTeaLab <dotsTeaLab@gmai.com>
2022-07-14 14:20:23 +0200

    artist shortcut padding adjusted

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/RecsplanationHeadingComponentBinder.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/RecsplanationHeadingComponentBinder.kt
@@ -1,13 +1,15 @@
 package bruhcollective.itaysonlab.jetispot.ui.dac.components_home
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
-import androidx.compose.material3.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,7 +19,6 @@ import androidx.compose.ui.unit.sp
 import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
 import bruhcollective.itaysonlab.jetispot.ui.shared.MediumText
 import bruhcollective.itaysonlab.jetispot.ui.shared.PreviewableAsyncImage
-import bruhcollective.itaysonlab.jetispot.ui.shared.SubtextOverline
 import com.spotify.home.dac.component.v1.proto.RecsplanationHeadingComponent
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -26,7 +27,7 @@ fun RecsplanationHeadingComponentBinder(
   navController: LambdaNavigationController,
   item: RecsplanationHeadingComponent
 ) {
-  Card(shape = RoundedCornerShape(32.dp), modifier = Modifier.padding(start = 12.dp, top = 16.dp, bottom = 16.dp)) {
+  Card(shape = RoundedCornerShape(32.dp), modifier = Modifier.padding(start = 12.dp, top = 32.dp, bottom = 16.dp)) {
     Row(
       Modifier
         .clickable { navController.navigate(item.navigateUri) }
