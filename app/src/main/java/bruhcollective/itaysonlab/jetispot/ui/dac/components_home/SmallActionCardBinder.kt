e3b4cdf184b8157bd7bac691bc7a42769764e702
DotsTeaLab <dotsTeaLab@gmai.com>
2022-07-14 21:00:25 +0200

    updated new release large card design

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/SmallActionCardBinder.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/SmallActionCardBinder.kt
@@ -2,17 +2,17 @@ package bruhcollective.itaysonlab.jetispot.ui.dac.components_home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
-import bruhcollective.itaysonlab.jetispot.ui.ext.compositeSurfaceElevation
-import bruhcollective.itaysonlab.jetispot.ui.shared.MediumText
 import bruhcollective.itaysonlab.jetispot.ui.shared.PreviewableAsyncImage
 import bruhcollective.itaysonlab.jetispot.ui.shared.Subtext
 import bruhcollective.itaysonlab.jetispot.ui.shared.dynamic_blocks.DynamicLikeButton
@@ -31,29 +31,38 @@ fun SmallActionCardBinder(
   imagePlaceholder: String,
   playCommand: PlayCommand
 ) {
-  Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.compositeSurfaceElevation(3.dp)), modifier = Modifier
-    .padding(horizontal = 16.dp)
-    .height(120.dp)
-    .fillMaxWidth()
-    .clickable {
-      navController.navigate(navigateUri)
-    }) {
-    Row {
-      PreviewableAsyncImage(imageUrl = imageUri, placeholderType = imagePlaceholder, modifier = Modifier
-        .fillMaxHeight()
-        .width(120.dp))
-      Box(
-        Modifier
-          .fillMaxSize()
-          .padding(horizontal = 16.dp, vertical = 12.dp)) {
-        Column(Modifier.align(Alignment.TopStart)) {
-          MediumText(text = title)
+  Card(
+    modifier = Modifier
+      .padding(horizontal = 16.dp)
+      .height(128.dp)
+      .fillMaxWidth()
+      .clip(RoundedCornerShape(32.dp))
+      .clickable { navController.navigate(navigateUri) }
+  ) {
+    Row(Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)) {
+      PreviewableAsyncImage(
+        imageUrl = imageUri,
+        placeholderType = imagePlaceholder,
+        modifier = Modifier
+          .fillMaxHeight()
+          .padding(bottom = 16.dp)
+          .clip(shape = RoundedCornerShape(16.dp))
+      )
+
+      Column(
+        verticalArrangement = Arrangement.SpaceBetween
+      ) {
+        Column(Modifier.padding(start = 16.dp)) {
+          Text(text = title, fontSize = 16.sp, fontWeight = FontWeight.Medium)
           Subtext(text = subtitle)
         }
 
-        Box(modifier = Modifier.offset(y = 4.dp).fillMaxWidth().align(Alignment.BottomStart)) {
-          DynamicLikeButton(objectUrl = likeUri, Modifier.offset(x = (-8).dp).size(42.dp).align(Alignment.CenterStart))
-          DynamicPlayButton(command = playCommand, Modifier.offset(x = 8.dp).size(42.dp).align(Alignment.CenterEnd))
+        Row(
+          modifier = Modifier.fillMaxWidth().padding(start = 4.dp, top = 6.dp),
+          horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+          DynamicLikeButton(objectUrl = likeUri)
+          DynamicPlayButton(command = playCommand)
         }
       }
     }
