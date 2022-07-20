86e0ff29b0e5a6780b51b057ad13dc1c1d2a0a9d
DotsTeaLab <dotsTeaLab@gmai.com>
2022-07-20 17:56:47 +0200

    large new release card design adjustments

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/SmallActionCardBinder.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/SmallActionCardBinder.kt
@@ -6,22 +6,26 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
 import bruhcollective.itaysonlab.jetispot.ui.shared.PreviewableAsyncImage
-import bruhcollective.itaysonlab.jetispot.ui.shared.Subtext
 import bruhcollective.itaysonlab.jetispot.ui.shared.dynamic_blocks.DynamicLikeButton
 import bruhcollective.itaysonlab.jetispot.ui.shared.dynamic_blocks.DynamicPlayButton
 import com.spotify.dac.player.v1.proto.PlayCommand
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalTextApi::class)
 @Composable
 fun SmallActionCardBinder(
   navController: LambdaNavigationController,
@@ -62,8 +66,24 @@ fun SmallActionCardBinder(
         verticalArrangement = Arrangement.SpaceBetween
       ) {
         Column(Modifier.padding(start = 16.dp)) {
-          Text(text = title, fontSize = 16.sp, fontWeight = FontWeight.Medium)
-          Subtext(text = subtitle)
+          Text(
+            text = title,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            maxLines = 2,
+            style = TextStyle(platformStyle = PlatformTextStyle(false))
+          )
+          Text(
+            text = subtitle,
+            fontSize = 12.sp,
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.onSurfaceVariant.copy(0.7f),
+            maxLines = 2,
+            style = TextStyle(platformStyle = PlatformTextStyle(false)),
+            modifier = Modifier.padding(top = 4.dp)
+          )
         }
 
         Row(
