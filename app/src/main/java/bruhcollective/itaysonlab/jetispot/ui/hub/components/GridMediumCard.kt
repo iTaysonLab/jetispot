4226ed21fb595bd6b5f6def5810805d34d2e1d63
Al-Alig <101019310+Al-Alig@users.noreply.github.com>
2022-07-26 01:15:27 +0300

    changed GridMediumCard

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/GridMediumCard.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/GridMediumCard.kt
@@ -2,46 +2,92 @@ package bruhcollective.itaysonlab.jetispot.ui.hub.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
+import androidx.compose.ui.unit.sp
 import bruhcollective.itaysonlab.jetispot.core.objs.hub.HubItem
+import bruhcollective.itaysonlab.jetispot.ui.LambdaNavigationController
 import bruhcollective.itaysonlab.jetispot.ui.hub.HubScreenDelegate
 import bruhcollective.itaysonlab.jetispot.ui.hub.clickableHub
-import bruhcollective.itaysonlab.jetispot.ui.shared.MediumText
 import bruhcollective.itaysonlab.jetispot.ui.shared.PreviewableAsyncImage
-import bruhcollective.itaysonlab.jetispot.ui.shared.Subtext
-import coil.compose.AsyncImage
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun GridMediumCard(
   navController: LambdaNavigationController,
   delegate: HubScreenDelegate,
   item: HubItem
 ) {
-  val size = 160.dp
-  
-  Column(Modifier.fillMaxWidth().clickableHub(navController, delegate, item)) {
-    var drawnTitle = false
+  Surface(
+    color = MaterialTheme.colorScheme.surfaceVariant,
+    shape = RoundedCornerShape(20.dp),
+    modifier = Modifier
+      .height(264.dp)
+      .width(164.dp)
+      .padding(6.dp)
+  ) {
+    Column(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = Modifier.width(172.dp).clickableHub(navController, delegate, item).padding(14.dp)
+    ) {
+      var drawnTitle = false
+      Surface(color = MaterialTheme.colorScheme.surfaceVariant) {
+        PreviewableAsyncImage(
+          imageUrl = item.images?.main?.uri,
+          placeholderType = item.images?.main?.placeholder,
+          modifier = Modifier
+            .weight(1f)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(12.dp))
+        )
+      }
 
-    PreviewableAsyncImage(imageUrl = item.images?.main?.uri, placeholderType = item.images?.main?.placeholder, modifier = Modifier.size(size).clip(
-      RoundedCornerShape(if (item.images?.main?.isRounded == true) 12.dp else 0.dp)
-    ).align(Alignment.CenterHorizontally))
+      Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+        drawnTitle = true
+        androidx.compose.material3.Text(
+          text = item.text!!.title!!,
+          fontSize = 16.sp,
+          maxLines = 1,
+          overflow = TextOverflow.Ellipsis,
+          style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+        )
 
-    if (!item.text?.title.isNullOrEmpty()) {
-      drawnTitle = true
-      MediumText(item.text!!.title!!, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth().padding(top = 8.dp).padding(horizontal = 16.dp))
-    }
-
-    if (!item.text?.subtitle.isNullOrEmpty()) {
-      Subtext(item.text!!.subtitle!!, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth().padding(top = if (drawnTitle) 2.dp else 8.dp, bottom = 12.dp).padding(horizontal = 16.dp))
-    } else if (!item.text?.description.isNullOrEmpty()) {
-      Subtext(item.text!!.description!!, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth().padding(top = if (drawnTitle) 2.dp else 8.dp, bottom = 12.dp).padding(horizontal = 16.dp))
+        if (!item.text?.subtitle.isNullOrEmpty()) {
+          androidx.compose.material.Text(
+            item.text!!.subtitle!!,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+            maxLines = 1,
+            style = TextStyle(platformStyle = PlatformTextStyle(false)),
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f),
+            modifier = Modifier.fillMaxWidth()
+              .padding(top = if (drawnTitle) 4.dp else 8.dp, bottom = 12.dp)
+          )
+        } else if (!item.text?.description.isNullOrEmpty()) {
+          androidx.compose.material.Text(
+            item.text!!.description!!,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+            maxLines = 1,
+            style = TextStyle(platformStyle = PlatformTextStyle(false)),
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f),
+            modifier = Modifier.fillMaxWidth()
+              .padding(top = if (drawnTitle) 4.dp else 8.dp, bottom = 12.dp)
+          )
+        }
+      }
     }
   }
 }
