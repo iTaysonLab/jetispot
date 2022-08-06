7ca93b0d94b4057e052d26ee05f357c4a8788146
DotsTeaLab <dotstealab@gmail.com>
2022-08-07 01:51:01 +0200

    AlbumTrackRow: edited fonts, added divider

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/AlbumTrackRow.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/AlbumTrackRow.kt
@@ -5,53 +5,75 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import bruhcollective.itaysonlab.jetispot.core.objs.hub.HubItem
 import bruhcollective.itaysonlab.jetispot.ui.hub.HubScreenDelegate
 import bruhcollective.itaysonlab.jetispot.ui.hub.clickableHub
-import bruhcollective.itaysonlab.jetispot.ui.shared.MediumText
-import bruhcollective.itaysonlab.jetispot.ui.shared.Subtext
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun AlbumTrackRow(
   delegate: HubScreenDelegate,
   item: HubItem
 ) {
-  Row(horizontalArrangement = Arrangement.SpaceBetween) {
-    Column(
-      Modifier
-        .clickableHub(delegate, item)
-        .fillMaxWidth(0.865f)
-        .padding(horizontal = 16.dp, vertical = 12.dp)) {
-      var drawnTitle = false
+  Column {
+    Row(
+      horizontalArrangement = Arrangement.SpaceBetween,
+      modifier = Modifier.clickableHub(delegate, item)
+    ) {
+      Column(Modifier.fillMaxWidth(0.865f).padding(horizontal = 16.dp, vertical = 16.dp)) {
+        var drawnTitle = false
 
-      if (!item.text?.title.isNullOrEmpty()) {
-        drawnTitle = true
-        MediumText(item.text!!.title!!, fontWeight = FontWeight.Normal)
+        if (!item.text?.title.isNullOrEmpty()) {
+          drawnTitle = true
+          Text(
+            item.text!!.title!!,
+            fontSize = 16.sp,
+            maxLines = 1,
+            style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+          )
+        }
+
+        if (!item.text?.subtitle.isNullOrEmpty()) {
+          Text(
+            item.text!!.subtitle!!,
+            fontSize = 14.sp,
+            maxLines = 1,
+            fontWeight = FontWeight.Medium,
+            style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false)),
+            modifier = Modifier
+              .padding(top = 4.dp)
+              .alpha(0.7f)
+          )
+        }
       }
 
-      if (!item.text?.subtitle.isNullOrEmpty()) {
-        Subtext(
-          item.text!!.subtitle!!,
-          modifier = Modifier.padding(top = if (drawnTitle) 4.dp else 8.dp)
+      IconButton(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+          .fillMaxWidth(1f)
+          .align(CenterVertically)
+      ) {
+        Icon(
+          imageVector = Icons.Default.MoreVert,
+          contentDescription = "Options for ${item.text!!.title!!} by ${item.text!!.subtitle!!}",
+          tint = MaterialTheme.colorScheme.onBackground
         )
       }
     }
 
-    IconButton(
-      onClick = { /*TODO*/ },
-      modifier = Modifier.fillMaxWidth(1f).align(CenterVertically)
-    ) {
-      Icon(
-        imageVector = Icons.Default.MoreVert,
-        contentDescription = "Options for ${item.text!!.title!!} by ${item.text!!.subtitle!!}",
-        tint = MaterialTheme.colorScheme.onBackground
-      )
-    }
+    Divider(modifier = Modifier.padding(horizontal = 16.dp))
   }
 }
