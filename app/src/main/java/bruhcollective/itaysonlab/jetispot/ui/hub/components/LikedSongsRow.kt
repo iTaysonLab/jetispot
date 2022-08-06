b9f49db4d5ff61ff14f7ec2b7e30caab6fbe8f13
DotsTeaLab <dotstealab@gmail.com>
2022-08-06 18:30:49 +0200

    Liked songs row on artist page: adjusted fonts

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/LikedSongsRow.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/LikedSongsRow.kt
@@ -7,24 +7,29 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import bruhcollective.itaysonlab.jetispot.core.objs.hub.HubEvent
 import bruhcollective.itaysonlab.jetispot.core.objs.hub.HubItem
 import bruhcollective.itaysonlab.jetispot.ui.hub.HubScreenDelegate
 import bruhcollective.itaysonlab.jetispot.ui.hub.clickableHub
-import bruhcollective.itaysonlab.jetispot.ui.shared.MediumText
-import bruhcollective.itaysonlab.jetispot.ui.shared.Subtext
 import kotlinx.coroutines.launch
 import xyz.gianlu.librespot.metadata.ArtistId
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun LikedSongsRow(
   delegate: HubScreenDelegate,
@@ -68,8 +73,20 @@ fun LikedSongsRow(
           .align(Alignment.CenterVertically)
           .padding(start = 16.dp)
       ) {
-        MediumText(item.text!!.title!!, fontWeight = FontWeight.Normal)
-        Subtext(likedSongsInfo.value, modifier = Modifier.padding(top = 4.dp))
+        Text(
+          item.text!!.title!!,
+          fontSize = 16.sp,
+          maxLines = 1,
+          style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+        )
+        Text(
+          likedSongsInfo.value,
+          fontSize = 14.sp,
+          maxLines = 1,
+          fontWeight = FontWeight.Medium,
+          style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false)),
+          modifier = Modifier.padding(top = 4.dp).alpha(0.7f)
+        )
       }
     }
   }
