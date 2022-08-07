028afe9500254399f91f7d5b574c248ea13fa7e1
DotsTeaLab <dotstealab@gmail.com>
2022-08-07 21:01:37 +0200

    Popular releases item in artist screen: redesigned

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/LargerRow.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/LargerRow.kt
@@ -1,16 +1,16 @@
 package bruhcollective.itaysonlab.jetispot.ui.hub.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import bruhcollective.itaysonlab.jetispot.core.objs.hub.HubItem
 import bruhcollective.itaysonlab.jetispot.ui.hub.HubScreenDelegate
 import bruhcollective.itaysonlab.jetispot.ui.hub.clickableHub
@@ -26,20 +26,36 @@ fun LargerRow (
   // Popular releases
   Row(
     Modifier
-      .clickableHub(delegate, item)
-      .padding(horizontal = 16.dp, vertical = 2.dp)) {
-    PreviewableAsyncImage(
-      imageUrl = item.images?.main?.uri,
-      placeholderType = item.images?.main?.placeholder,
-      modifier = Modifier
-        .size(78.dp)
-        .padding(end = 16.dp)
-        .padding(vertical = 8.dp)
-        .clip(RoundedCornerShape(8.dp))
-    )
-    Column(Modifier.align(Alignment.CenterVertically)) {
-      MediumText(item.text!!.title!!, fontWeight = FontWeight.Normal, modifier = Modifier.padding(bottom = 4.dp))
-      Subtext(item.text.subtitle!!)
+      .height(132.dp)
+      .fillMaxWidth()
+      .padding(horizontal = 16.dp, vertical = 8.dp)
+      .clip(RoundedCornerShape(24.dp))
+      .background(MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp))
+  ) {
+    Row(Modifier.clickableHub(delegate, item).fillMaxWidth()) {
+      PreviewableAsyncImage(
+        imageUrl = item.images?.main?.uri,
+        placeholderType = item.images?.main?.placeholder,
+        modifier = Modifier
+          .padding(16.dp)
+          .clip(RoundedCornerShape(10.dp))
+          .size(84.dp)
+          .aspectRatio(1f)
+      )
+      Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+          .padding(vertical = 32.dp)
+          .fillMaxHeight()
+      ) {
+        MediumText(
+          item.text!!.title!!,
+          fontWeight = FontWeight.Normal,
+          modifier = Modifier.padding(bottom = 4.dp),
+          fontSize = 18.sp
+        )
+        Subtext(item.text.subtitle!!, fontSize = 12.sp)
+      }
     }
   }
 }
