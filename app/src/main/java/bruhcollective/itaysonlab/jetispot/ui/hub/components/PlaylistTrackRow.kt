36f35f1b952b1729a336afdf0706a413225cd6f2
DotsTeaLab <dotstealab@gmail.com>
2022-08-07 02:02:52 +0200

    PlaylistTrackRow: diviver added

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/PlaylistTrackRow.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/PlaylistTrackRow.kt
@@ -6,73 +6,96 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import bruhcollective.itaysonlab.jetispot.core.objs.hub.HubItem
 import bruhcollective.itaysonlab.jetispot.ui.hub.HubScreenDelegate
 import bruhcollective.itaysonlab.jetispot.ui.hub.clickableHub
-import bruhcollective.itaysonlab.jetispot.ui.shared.MediumText
 import bruhcollective.itaysonlab.jetispot.ui.shared.PreviewableAsyncImage
-import bruhcollective.itaysonlab.jetispot.ui.shared.Subtext
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun PlaylistTrackRow(
   delegate: HubScreenDelegate,
   item: HubItem
 ) {
-  Row(
-    Modifier
-      .clickableHub(delegate, item)
-      .fillMaxWidth()
-      .padding(start = 16.dp, top = 8.dp, bottom = 8.dp),
-    horizontalArrangement = Arrangement.SpaceBetween
-  ) {
-    Row(modifier = Modifier.fillMaxWidth(0.865f)) {
-      PreviewableAsyncImage(
-        imageUrl = item.images?.main?.uri,
-        placeholderType = "track",
-        modifier = Modifier
-          .align(Alignment.CenterVertically)
-          .size(48.dp)
-          .clip(RoundedCornerShape(8.dp))
-      )
+  Column {
+    Row(
+      Modifier
+        .clickableHub(delegate, item)
+        .fillMaxWidth()
+        .padding(start = 16.dp, top = 16.dp, bottom = 16.dp),
+      horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+      Row(modifier = Modifier.fillMaxWidth(0.865f)) {
+        PreviewableAsyncImage(
+          imageUrl = item.images?.main?.uri,
+          placeholderType = "track",
+          modifier = Modifier
+            .align(Alignment.CenterVertically)
+            .size(48.dp)
+            .clip(RoundedCornerShape(8.dp))
+        )
 
-      Column(
-        Modifier
-          .padding(start = 16.dp)
+        Column(
+          Modifier
+            .padding(start = 16.dp)
+            .align(Alignment.CenterVertically)
+        ) {
+          var drawnTitle = false
+
+          if (!item.text?.title.isNullOrEmpty()) {
+            drawnTitle = true
+            Text(
+              item.text!!.title!!,
+              fontWeight = FontWeight.Normal,
+              fontSize = 16.sp,
+              maxLines = 1,
+              style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+            )
+          }
+
+          if (!item.text?.subtitle.isNullOrEmpty()) {
+            Text(
+              item.text!!.subtitle!!,
+              fontSize = 14.sp,
+              maxLines = 1,
+              fontWeight = FontWeight.Medium,
+              style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false)),
+              modifier = Modifier
+                .padding(top = if (drawnTitle) 4.dp else 8.dp)
+                .alpha(0.7f)
+            )
+          }
+        }
+      }
+
+      IconButton(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+          .fillMaxWidth(1f)
           .align(Alignment.CenterVertically)
       ) {
-        var drawnTitle = false
-
-        if (!item.text?.title.isNullOrEmpty()) {
-          drawnTitle = true
-          MediumText(item.text!!.title!!, fontWeight = FontWeight.Normal)
-        }
-
-        if (!item.text?.subtitle.isNullOrEmpty()) {
-          Subtext(
-            item.text!!.subtitle!!,
-            modifier = Modifier.padding(top = if (drawnTitle) 4.dp else 8.dp),
-            maxLines = 1,
-          )
-        }
+        Icon(
+          imageVector = Icons.Default.MoreVert,
+          contentDescription = "Options for ${item.text?.title} by ${item.text?.subtitle}",
+          tint = MaterialTheme.colorScheme.onBackground
+        )
       }
     }
 
-    IconButton(
-      onClick = { /*TODO*/ },
-      modifier = Modifier.fillMaxWidth(1f).align(Alignment.CenterVertically)
-    ) {
-      Icon(
-        imageVector = Icons.Default.MoreVert,
-        contentDescription = "Options for ${item.text?.title} by ${item.text?.subtitle}",
-        tint = MaterialTheme.colorScheme.onBackground
-      )
-    }
+    Divider(Modifier.padding(horizontal = 16.dp).alpha(0.3f))
   }
 }
