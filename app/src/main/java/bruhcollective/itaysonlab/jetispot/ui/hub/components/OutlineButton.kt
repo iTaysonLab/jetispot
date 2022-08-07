72194ef49142f968f7ec135256c4afac56137581
DotsTeaLab <dotstealab@gmail.com>
2022-08-07 21:01:06 +0200

    Open discography button: redesigned

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/OutlineButton.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/hub/components/OutlineButton.kt
@@ -1,31 +1,48 @@
 package bruhcollective.itaysonlab.jetispot.ui.hub.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ChevronRight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import bruhcollective.itaysonlab.jetispot.core.objs.hub.HubItem
 import bruhcollective.itaysonlab.jetispot.ui.hub.HubScreenDelegate
 import bruhcollective.itaysonlab.jetispot.ui.hub.clickableHub
-import bruhcollective.itaysonlab.jetispot.ui.shared.MediumText
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun OutlineButton(
-    delegate: HubScreenDelegate,
-    item: HubItem
+  delegate: HubScreenDelegate,
+  item: HubItem
 ) {
-    Box(Modifier.clickableHub(delegate, item).padding(16.dp)) {
-        Row(Modifier.align(Alignment.Center)) {
-            MediumText(text = item.text?.title!!, modifier = Modifier.align(Alignment.CenterVertically))
-            Icon(Icons.Rounded.ChevronRight, null, tint = MaterialTheme.colorScheme.onSurface, modifier = Modifier.padding(start = 2.dp).size(20.dp).align(Alignment.CenterVertically))
-        }
+  Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
+    Box(
+      Modifier
+        .height(40.dp)
+        .clip(RoundedCornerShape(64.dp))
+        .background(MaterialTheme.colorScheme.secondaryContainer)
+        .clickableHub(delegate, item),
+      contentAlignment = Alignment.Center
+    ) {
+      Text(
+        text = item.text?.title!!,
+        style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false)),
+        fontWeight = FontWeight.Medium,
+        color = MaterialTheme.colorScheme.onSecondaryContainer,
+        modifier = Modifier.padding(horizontal = 24.dp)
+      )
     }
+  }
 }
