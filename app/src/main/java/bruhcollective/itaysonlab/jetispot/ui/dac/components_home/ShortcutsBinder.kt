80d557b476c9547578f9346d25b9b379d46fdbf2
Gabriel Fontán <gabilessto@gmail.com>
2022-12-09 22:22:18 +0100

    Changed "Choose an artist" string to app "R" denotation and fixed Hub shortcuts not being fully clickable (before adjusted to text)

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/ShortcutsBinder.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/ui/dac/components_home/ShortcutsBinder.kt
@@ -16,40 +16,96 @@ import com.spotify.home.dac.component.v1.proto.*
 
 @Composable
 fun ShortcutsBinder(
-  item: ShortcutsSectionComponent
+    item: ShortcutsSectionComponent
 ) {
-  item.shortcutsList.map { it.dynamicUnpack() }.chunked(2).forEachIndexed { idx, pairs ->
-    Row(Modifier.padding(horizontal = 16.dp).padding(bottom = if (idx != item.shortcutsList.lastIndex / 2) 8.dp else 0.dp)) {
-      pairs.forEachIndexed { xIdx, xItem ->
-        Box(Modifier.weight(1f).padding(end = if (xIdx == 0) 8.dp else 0.dp)) {
-          when (xItem) {
-            is AlbumCardShortcutComponent -> ShortcutComponentBinder(xItem.navigateUri, xItem.imageUri, "album", xItem.title)
-            is PlaylistCardShortcutComponent -> ShortcutComponentBinder(xItem.navigateUri, xItem.imageUri, "playlist", xItem.title)
-            is ShowCardShortcutComponent -> ShortcutComponentBinder(xItem.navigateUri, xItem.imageUri, "podcasts", xItem.title)
-            is ArtistCardShortcutComponent -> ShortcutComponentBinder(xItem.navigateUri, xItem.imageUri, "artist", xItem.title)
-            is EpisodeCardShortcutComponent -> ShortcutComponentBinder(xItem.navigateUri, xItem.imageUri, "podcasts", xItem.title)
-          }
+    item.shortcutsList.map { it.dynamicUnpack() }.chunked(2).forEachIndexed { idx, pairs ->
+        Row(
+          Modifier
+            .padding(horizontal = 16.dp)
+            .padding(bottom = if (idx != item.shortcutsList.lastIndex / 2) 8.dp else 0.dp)
+        ) {
+            pairs.forEachIndexed { xIdx, xItem ->
+                Box(
+                  Modifier
+                    .weight(1f)
+                    .padding(end = if (xIdx == 0) 8.dp else 0.dp)) {
+                    when (xItem) {
+                        is AlbumCardShortcutComponent -> ShortcutComponentBinder(
+                            xItem.navigateUri,
+                            xItem.imageUri,
+                            "album",
+                            xItem.title
+                        )
+                        is PlaylistCardShortcutComponent -> ShortcutComponentBinder(
+                            xItem.navigateUri,
+                            xItem.imageUri,
+                            "playlist",
+                            xItem.title
+                        )
+                        is ShowCardShortcutComponent -> ShortcutComponentBinder(
+                            xItem.navigateUri,
+                            xItem.imageUri,
+                            "podcasts",
+                            xItem.title
+                        )
+                        is ArtistCardShortcutComponent -> ShortcutComponentBinder(
+                            xItem.navigateUri,
+                            xItem.imageUri,
+                            "artist",
+                            xItem.title
+                        )
+                        is EpisodeCardShortcutComponent -> ShortcutComponentBinder(
+                            xItem.navigateUri,
+                            xItem.imageUri,
+                            "podcasts",
+                            xItem.title
+                        )
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
 
 @Composable
 private fun ShortcutComponentBinder(
-  navigateUri: String,
-  imageUrl: String,
-  imagePlaceholder: String,
-  title: String
+    navigateUri: String,
+    imageUrl: String,
+    imagePlaceholder: String,
+    title: String
 ) {
-  Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.compositeSurfaceElevation(3.dp)), modifier = Modifier.height(56.dp).fillMaxWidth()) {
-    Row(Modifier.navClickable { navController ->
-      navController.navigate(navigateUri)
-    }) {
-      PreviewableAsyncImage(imageUrl = imageUrl, placeholderType = imagePlaceholder, modifier = Modifier.size(56.dp))
-      Text(title, fontSize = 13.sp, lineHeight = 18.sp, maxLines = 2, overflow = TextOverflow.Ellipsis, modifier = Modifier.align(
-        Alignment.CenterVertically).padding(horizontal = 8.dp))
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.compositeSurfaceElevation(
+                3.dp
+            )
+        ), modifier = Modifier
+        .height(56.dp)
+        .fillMaxWidth()
+    ) {
+        Row(Modifier.navClickable { navController ->
+            navController.navigate(navigateUri)
+        }) {
+            PreviewableAsyncImage(
+                imageUrl = imageUrl,
+                placeholderType = imagePlaceholder,
+                modifier = Modifier.size(56.dp)
+            )
+            Text(
+                title,
+                fontSize = 13.sp,
+                lineHeight = 18.sp,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                  .align(
+                    Alignment.CenterVertically
+                  )
+                  .padding(horizontal = 8.dp)
+                    .fillMaxWidth(),
+
+            )
+        }
     }
-  }
 }
 
